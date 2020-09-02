45efc4c463eb50c878b36e5101e2671711aaf03d
rafael.g.magalhaes <rafael.g.magalhaes@animaeducacao.com.br>
2020-09-02 10:12:25 -0300

    Add media queries in screen Dados Entrega

--- src/views/DadosEntrega/DadosEntrega.vue
+++ src/views/DadosEntrega/DadosEntrega.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="form" class="container">
     <h2>Dados Entrega</h2>
-    <input type="text" name="nome" id="nome" placeholder="Nome"/>
-    <input type="number" name="tel" id="tel" placeholder="Telefone" />
-    <input type="text" name="logradouro" id="logradouro" placeholder="Logradouro" />
-    <input type="number" name="num" id="num" placeholder="Número" />
-    <button @click="enviarCozinha()">Enviar Pedido</button>
-
-  </div>
+    
+      <input type="text" name="nome" id="nome" placeholder="Nome" />
+      <input type="number" name="tel" id="tel" placeholder="Telefone" />
+      <input
+        type="text"
+        name="logradouro"
+        id="logradouro"
+        placeholder="Logradouro"
+      />
+      <input type="number" name="num" id="num" placeholder="Número" />
+      <button @click="enviarCozinha()">Enviar Pedido</button>
+    </div>
+ 
 </template>
 <script>
 export default {
-    methods:{
-        enviarCozinha(){
-            alert("Pedido enviado a nossa cozinha.")
-        }
-    }
-}
+  methods: {
+    enviarCozinha() {
+      alert("Pedido enviado a nossa cozinha.");
+    },
+  },
+};
 </script>
 <style lang="scss" scoped>
-@import './DadosEntrega.scss';
+@import "./DadosEntrega.scss";
 </style>
